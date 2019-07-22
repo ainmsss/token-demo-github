5bbd5718290cf47a3bac3af2d08798428cd5df6e
崔晨 <chen.cui@albus.net.cn>
2019-07-23 00:42:10 +0800

    d

--- src/main/java/com/ccsia/demo/controller/ApplyController.java
+++ src/main/java/com/ccsia/demo/controller/ApplyController.java
@@ -24,7 +24,7 @@ public class ApplyController {
 
     @Autowired AuthService authService;
 
-    @RequestMapping(value = "/token")
+    @RequestMapping(value = "/token.json")
     public Token getToken(String client_id, String client_secret){
         Token token = new Token();
         if(StringUtils.isBlank(client_id) || StringUtils.isBlank(client_secret)) {
@@ -40,7 +40,7 @@ public class ApplyController {
         return token;
     }
 
-    @PostMapping(value = "/applicants")
+    @PostMapping(value = "/applicants.json")
     public Result getToken(@RequestBody ApplicantParams params){
         if(!authService.checkToken(params.getClient_id(), params.getAccess_token()))
             return ResultBuilder.error(Err.EXPIRED_TOKEN.getErrCode(), Err.EXPIRED_TOKEN.getErrMsg());
